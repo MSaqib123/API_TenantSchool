ce321c39e57c7530e6cc129e1cd28072acefd3a7
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-05 00:03:44 +0500

    4 -School Entity Definition

---
+++ Domain/School.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain
+{
+    public class School
+    {
+        #region Base Prop
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EstablishedDate { get; set; }
+        #endregion
+
+
+        #region Multi_Tenant Prop
+
+        #endregion
+    }
+}
