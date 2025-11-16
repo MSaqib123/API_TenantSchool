a4489d8cc1666951a177f2f1eb352f99a9804098
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-16 18:57:05 +0500

    12 -Application Role and Role Claim

---
+++ Infrastructure/Identity/Models/ApplicationRoleClaim.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Models
+{
+    public class ApplicationRoleClaim : IdentityRoleClaim<string>
+    {
+        // RolesClaim is wn as "permissions" in the system
+        // its used to define what actions a role can perform
+        // e.g., "CanEditUsers", "CanDeletePosts", etc.
+        // These claims are assigned to roles and then users inherit these claims through their assigned roles.
+
+        public string Description { get; set; }
+        public string Group { get; set; }
+
+    }
+}
