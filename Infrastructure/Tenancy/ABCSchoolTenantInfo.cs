499461d93938d4668138539c2efb4660eb308ea6
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-16 16:58:05 +0500

    6 -Tenant Entity Definition

---
+++ Infrastructure/Tenancy/ABCSchoolTenantInfo.cs
@@ -0,0 +1,26 @@
+﻿using Finbuckle.MultiTenant;
+using Finbuckle.MultiTenant.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Tenancy
+{
+    public class ABCSchoolTenantInfo : ITenantInfo
+    {
+        public string Id { get; set; }
+        public string Identifier { get; set; }
+        public string Name { get; set; }
+        //====== database of own tenant =====
+        public string ConnectionString { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //validity of tenant and active status
+        public DateTime ValidUpTo { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
